832b421f73aa94c6a5deb9a0536c99bf963101bc
blevon24 <brianna.levon24@gmail.com>
2025-04-04 11:25:45 -0400

    worked on installation.sh

--- sys-installs/installation.sh
+++ sys-installs/installation.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
+loop=true
 
+while $loop; do
+	# Check is user is root
+	if [ "$EUID" -ne 0 ]; then
+		echo "To run this script you must be the root user or use sudo."
+		echo "Exiting program"
+		exit
+	#elif []
+	#then
+		#echo "hey"
+	else
+		#continue
+		echo "you are root!"
+		exit
+
+	fi
+
+	# Provides installation information and prompts for confirmation
+	echo "hi"
+
+	# Checks if software already exists on the terminal or if the name already exists
+
+	# Silently installs software
+	#TODO: install program
+	#TODO: check program installed properly
+
+done
+
+exit
