9b09eafd0fa87f17f4bc4b17a10c041a9583129b
blevon24 <brianna.levon24@gmail.com>
2026-06-05 11:54:03 -0400

    Create music.sh

---
+++ applications/Music/music.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+loop=true
+
+menu() {
+  echo "Welcome to the Main Menu! Select an option."
+  echo "1) cmus"
+  echo "2) Help (displays this menu again)"
+  echo "3) Quit (exits the program)"
+}
+
+while true; do
+	# Displays the option menu for the user to decide which music packages they want installed and takes their choice/action and runs in through the case options.
+	menu()
+  read -p "Enter your choice [1-3]: \n" opt
+  
+  case "$opt" in
+      1)
+          sudo apt install cmus
+          echo "Package installation completed!"
+          ;;
+      2)
+          clear
+          menu()
+          ;;
+      3)
+          exit 0
+          ;;
+      *)
+          echo "Invalid option. Try Again."
+          ;;
+  esac
+done
+
+  '''
+	if [[ $confirm == "y" || $confirm == "Y" ]]; then
+		linkCheck1=$(readlink -f "$bashSymLink")
+
+		if [ -L ${bashSymLink} ]; then
+			echo "'${bashSymLink}' is already symbolically linked to '${linkCheck1}'"
+			echo -e "Exiting program\n"
+			loop=false
+		else
+			echo "Symbolically linking your .bashrc files"
+			ln -s -f ${bashrc} ${bashSymLink}
+			echo "Symlink Completed"
+			echo "NOTE: Don't forget to reload your shell environment by logging out and back in!"
+			echo -e "Exiting program\n"
+			loop=false
+		fi
+
+	elif [[ $confirm == "n" || $confirm == "N" ]]
+	then
+		# Prompts user if they want to symlink the .bash_aliases file in homefiles to the home directory and saves their answer as a variable
+		printf "Do you want to symbolically link your .bash_aliases file in your dotfiles repository to your home directory? (y/n) "
+		read -r confirm
+				
+		if [[ $confirm == "y" || $confirm == "Y" ]]; then
+			# Checks if .bash_aliases exists in the user's home directory before proceeding with creating a symlink. This prevents the program from accidentally creating a broken or dangling symlink if the file its linked to does not exist. Also, the program creates the .bash_aliases file if it detects no file is found in the home directory. 
+			if [ ! -f ${aliasesSymLink} ]; then
+				echo "File does not exist in Bash"
+				echo "Creating file now"
+
+				touch ${aliasesSymLink}
+				echo "Created .bash_aliases file"
+				echo "Check it worked before starting program again"
+				echo -e "Exiting program\n"
+				loop=false
+			else
+				echo "File exists" 
+				echo "Proceeding with symlink configuration"
+				linkCheck2=$(readlink -f "$aliasesSymLink")
+
+				if [ -L ${aliasesSymLink} ]; then
+					echo "'${aliasesSymLink}' is already symbolically linked to '${linkCheck2}'"
+					echo -e "Exiting program\n"
+					loop=false
+				else
+					echo "Symbolically linking your .bashrc files"
+					ln -s -f ${bashAliases} ${aliasesSymLink}
+					echo "Symlink Completed"
+					echo "NOTE: Don't forget to reload your shell environment by logging out and back in!"
+					echo -e "Exiting program\n"
+					loop=false
+				fi
+			fi
+			
+		elif [[ $confirm == "n" || $confirm == "N" ]]
+		then
+			echo -e "Exiting program\n"
+			loop=false
+
+		else
+			echo -e "Invalid input. Answer either needs to be 'y' or 'n'.\n"
+
+		fi
+	else
+		echo -e "Invalid input. Answer either needs to be 'y' or 'n'.\n"
+
+	fi
+done	
+'''
+
+exit
